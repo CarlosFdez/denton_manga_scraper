7b81396c8e1463d5d480da4c7d2bce3a960513bc
David Peter <david.a.peter@gmail.com>
2012-06-21 09:46:22 -0400

    Add redis and store new manga.

--- main.py
+++ main.py
@@ -6,21 +6,29 @@ class DentonBot(pyrc.Bot):
   def __init__(self, *args, **kwargs):
     super(DentonBot, self).__init__(*args, **kwargs)
     self.scraper = Scraper()
+    self.scraper.add_manga('One Piece', ['mangastream'])
+    self.scraper.add_manga('Bleach', ['mangastream'])
     self.scraper.add_manga('Naruto', ['mangastream'])
-    
+
   @hooks.command
   def bling(self, channel):
     self.message(channel, "yo")
-    
+
   @hooks.interval(15000)
   def scrape(self):
     results = self.scraper.scrape()
     for (name, chapter, link) in results:
-      msg = 'New %s: %s' % (name, link)
+      msg = 'New %s (%i): %s' % (name, chapter, link)
       self.message('#testchannel', msg)
-  
+
+  @hooks.command
+  def fetch_manga(self, channel):
+    results = self.scraper.get_manga()
+    for (name, chapter, link) in results:
+      self.message(channel, "%s %i: %s" % (name, chapter, link))
+
 if __name__ == '__main__':
-  bot = DentonBot('irc.synirc.net', 
+  bot = DentonBot('irc.synirc.net',
     nick='JCDenton',
     names=['JC', 'JCDenton', 'Denton', 'JCD'],
     realname='JC Denton Bot',
--- scraper.py
+++ scraper.py
@@ -1,3 +1,4 @@
+import redis
 import requests
 from bs4 import BeautifulSoup
 
@@ -13,21 +14,46 @@ def mangastream_manga_list():
 
 # TODO: Support more than just mangastream
 class Scraper(object):
-  def __init__(self, *args, **kwargs):
+  def __init__(self):
+    self.db = redis.StrictRedis(host='localhost', port=6379, db=0)
     self.manga = {}
     self.mangastream = []
-    
+
   def add_manga(self, name, sources):
     self.manga[name] = sources
     if 'mangastream' in sources:
       self.mangastream.append(name)
-    
+
   def scrape(self):
     results = []
     for (manga, link) in mangastream_manga_list():
       (name, chapter) = manga.rsplit(' ', 1)
       chapter = int(chapter)
       if name in self.mangastream:
-        results.append( (name, chapter, link) )
+        if self.is_new(name, chapter):
+          result = (name, chapter, link)
+          results.append(result)
+          self.record(result)
     return results
-       
+
+  def record(self, result):
+    (name, chapter, link) = result
+    chapter_key = "manga:%s:latest:chapter" % name
+    link_key = "manga:%s:latest:link" % name
+    self.db.set(chapter_key, chapter)
+    self.db.set(link_key, link)
+
+  def get_manga(self):
+    results = []
+    for name in self.manga.keys():
+      link = self.db.get("manga:%s:latest:link" % name)
+      if not link: continue
+      chapter = int(self.db.get("manga:%s:latest:chapter" % name))
+      results.append( (name, chapter, link) )
+    return results
+
+  def is_new(self, name, scraped_chapter):
+    chapter = self.db.get("manga:%s:latest:chapter" % name)
+    if chapter is None:
+      return True
+    return scraped_chapter > int(chapter)
